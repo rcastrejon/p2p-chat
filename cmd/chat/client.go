65d21256818bbb5345789002015a4df7d44fc00c
Rodrigo Castrejon <47370853+rcastrejon@users.noreply.github.com>
2024-02-25 23:29:02 -0600

    add comments to the client

--- cmd/chat/client.go
+++ cmd/chat/client.go
@@ -14,13 +14,15 @@ type ChatClient struct {
 }
 
 func NewChatClient(peerAddress string, localPort string) (*ChatClient, error) {
-	// Resolve the peer address
+	// Resolve the peer address, asume that the peer is already listening. The
+	// peer address should be in the form of "host:port".
 	peer, err := net.ResolveUDPAddr("udp4", peerAddress)
 	if err != nil {
 		return nil, err
 	}
 
-	// Create a packet connection
+	// Create a listen packet connection. This will listen for incoming udp
+	// packets on the specified port.
 	conn, err := net.ListenPacket("udp4", ":"+localPort)
 	if err != nil {
 		return nil, err
@@ -32,20 +34,25 @@ func NewChatClient(peerAddress string, localPort string) (*ChatClient, error) {
 		sub:  make(chan *pb.Message),
 	}
 
+	// Start a goroutine to listen for incoming messages without blocking the
+	// main thread.
 	go c.listenForMessages()
 
 	return c, nil
 }
 
+// Listens for udp packets and sends them to the pb.Message channel. This is a
+// blocking function, so it is meant to be run in a goroutine.
 func (c *ChatClient) listenForMessages() {
 	buf := make([]byte, 1024)
 	for {
-		n, _, err := c.conn.ReadFrom(buf)
-		if err != nil {
-			continue
-		}
+		// TODO: consider handling the address of the sender
+		n, _, _ := c.conn.ReadFrom(buf)
+
+		// We want to avoid blocking the listener, so we CAN spawn a goroutine to
+		// handle the message (probably not necessary for our small program).
 		m := pb.Message{}
-		err = proto.Unmarshal(buf[:n], &m)
+		err := proto.Unmarshal(buf[:n], &m)
 		if err != nil {
 			continue
 		}
@@ -58,11 +65,11 @@ func (c *ChatClient) Close() {
 }
 
 func (c *ChatClient) Send(message *pb.Message) error {
-	b, err := proto.Marshal(message)
+	data, err := proto.Marshal(message)
 	if err != nil {
 		return err
 	}
-	_, err = c.conn.WriteTo(b, c.peer)
+	_, err = c.conn.WriteTo(data, c.peer)
 	return err
 }
 
